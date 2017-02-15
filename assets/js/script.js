1f77341c821e7482c571052bc48bb3a5bedf9fcd
chipotleqdoba <tae.h.min@pwc.com>
2017-02-15 17:43:01 -0500

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,28 +15,30 @@ $("#submit_now").click(function(){
 	var att8 = $("#att8").val();
 	var att9 = $("#att9").val();
 	var att10 = $("#att10").val();
-	var att11 = $("#att11").val();
-	var att12 = $("#att12").val();
-	var att13 = $("#att13").val();
-	var att14 = $("#att14").val();
-	var att15 = $("#att15").val();
-	var att16 = $("#att16").val();
-	var att17 = $("#att17").val();
-	var att18 = $("#att18").val();
-	var att19 = $("#att19").val();
-	var att20 = $("#att20").val();
-	var att21 = $("#att21").val();
-	var att22 = $("#att22").val();
+	// var att11 = $("#att11").val();
+	// var att12 = $("#att12").val();
+	// var att13 = $("#att13").val();
+	// var att14 = $("#att14").val();
+	// var att15 = $("#att15").val();
+	// var att16 = $("#att16").val();
+	// var att17 = $("#att17").val();
+	// var att18 = $("#att18").val();
+	// var att19 = $("#att19").val();
+	// var att20 = $("#att20").val();
+	// var att21 = $("#att21").val();
+	// var att22 = $("#att22").val();
 	var priority = "";
-	var total = (+att1)+(+att2)+(+att3)+(+att4)+(+att5)+(+att6)+(+att7)+(+att8)+(+att9)+(+att10)+(+att11)+(+att12)+(+att13)+(+att14)+(+att15)+(+att16)+(+att17)+(+att18)+(+att19)+(+att20)+(+att21)+(+att22);
-	if(total <= 22){
+	// var total = (+att1)+(+att2)+(+att3)+(+att4)+(+att5)+(+att6)+(+att7)+(+att8)+(+att9)+(+att10)+(+att11)+(+att12)+(+att13)+(+att14)+(+att15)+(+att16)+(+att17)+(+att18)+(+att19)+(+att20)+(+att21)+(+att22);
+	var total = (+att1)+(+att2)+(+att3)+(+att4)+(+att5)+(+att6)+(+att7)+(+att8)+(+att9)+(+att10);
+	if(total <= 10){
 		priority = "Low";
-	}else if(total >22 && total < 44){
+	}else if(total >10 && total < 20){
 		priority = "Medium";
-	}else if(total >=44){
+	}else if(total >=20){
 		priority = "High";
 	}
-	$("#output_table").append("<tr><th>"+priority+"</th><th>"+input+"</th><td>"+att1+"</td><td>"+att2+"</td><td>"+att3+"</td><td>"+att4+"</td><td>"+att5+"</td><td>"+att6+"</td><td>"+att7+"</td><td>"+att8+"</td><td>"+att9+"</td><td>"+att10+"</td><td>"+att11+"</td><td>"+att12+"</td><td>"+att13+"</td><td>"+att14+"</td><td>"+att15+"</td><td>"+att16+"</td><td>"+att17+"</td><td>"+att18+"</td><td>"+att19+"</td><td>"+att20+"</td><td>"+att21+"</td><td>"+att22+"</td><td>"+total+"</td></tr>");
+	// $("#output_table").append("<tr><th>"+priority+"</th><th>"+input+"</th><td>"+att1+"</td><td>"+att2+"</td><td>"+att3+"</td><td>"+att4+"</td><td>"+att5+"</td><td>"+att6+"</td><td>"+att7+"</td><td>"+att8+"</td><td>"+att9+"</td><td>"+att10+"</td><td>"+att11+"</td><td>"+att12+"</td><td>"+att13+"</td><td>"+att14+"</td><td>"+att15+"</td><td>"+att16+"</td><td>"+att17+"</td><td>"+att18+"</td><td>"+att19+"</td><td>"+att20+"</td><td>"+att21+"</td><td>"+att22+"</td><td>"+total+"</td></tr>");
+	$("#output_table").append("<tr><th>"+priority+"</th><th>"+input+"</th><td>"+att1+"</td><td>"+att2+"</td><td>"+att3+"</td><td>"+att4+"</td><td>"+att5+"</td><td>"+att6+"</td><td>"+att7+"</td><td>"+att8+"</td><td>"+att9+"</td><td>"+att10+"</td><td>"+total+"</td></tr>");
 }
 });
 };
